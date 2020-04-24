d8fbf5951f036b34b147309abf82752f05e8f154
uyennguyen16900 <uyennguyen16900@gmail.com>
2020-04-23 22:02:23 -0700

    Finished all tests

--- src/routes/message.js
+++ src/routes/message.js
@@ -6,51 +6,79 @@ const Message = require('../models/message')
 
 /** Route to get all messages. */
 router.get('/', (req, res) => {
-    // TODO: Get all Message objects using `.find()`
-
-    // TODO: Return the Message objects as a JSON list
+  // Get all Message objects using `.find()`
+  Message.find().then(messages => {
+    return res.json({messages})
+  })
+  .catch((err) => {
+    throw err.message
+  })
 })
 
 /** Route to get one message by id. */
 router.get('/:messageId', (req, res) => {
-    // TODO: Get the Message object with id matching `req.params.id`
-    // using `findOne`
-
-    // TODO: Return the matching Message object as JSON
+  // Get the Message object with id matching `req.params.id`
+  // using `findOne`
+  Message.findOne({_id: req.params.messageId})
+  .then(result => {
+    // Return the matching Message object as JSON
+    return res.json(result)
+  })
+  .catch(err => {
+    throw err.message
+  })
 })
 
 /** Route to add a new message. */
 router.post('/', (req, res) => {
-    let message = new Message(req.body)
-    message.save()
-    .then(message => {
-        return User.findById(message.author)
-    })
-    .then(user => {
-        // console.log(user)
-        user.messages.unshift(message)
-        return user.save()
-    })
-    .then(() => {
-        return res.send(message)
-    }).catch(err => {
-        throw err.message
-    })
+  let message = new Message(req.body)
+  message.save()
+  .then(message => {
+    return User.findById(message.author)
+  })
+  .then(user => {
+    // console.log(user)
+    user.messages.unshift(message)
+    return user.save()
+  })
+  .then(() => {
+    return res.send(message)
+  }).catch(err => {
+    throw err.message
+  })
 })
 
 /** Route to update an existing message. */
 router.put('/:messageId', (req, res) => {
-    // TODO: Update the matching message using `findByIdAndUpdate`
+  // TODO: Update the matching message using `findByIdAndUpdate`
+  Message.findByIdAndUpdate(req.params.messageId, req.body).then(() => {
+    return Message.findOne({_id: req.params.messageId})
+  }).then((message) => {
+    // Return the updated Message object as JSON
+    return res.json({message})
+  }).catch((err) => {
+    throw err.message
+  })
 
-    // TODO: Return the updated Message object as JSON
 })
 
 /** Route to delete a message. */
 router.delete('/:messageId', (req, res) => {
-    // TODO: Delete the specified Message using `findByIdAndDelete`. Make sure
-    // to also delete the message from the User object's `messages` array
-
-    // TODO: Return a JSON object indicating that the Message has been deleted
+  // TODO: Delete the specified Message using `findByIdAndDelete`. Make sure
+  // to also delete the message from the User object's `messages` array
+  Message.findByIdAndDelete(req.params.messageId).then((result) => {
+    if (result === null) {
+      return res.json({message: 'Message does not exist.'})
+    }
+    return res.json({
+      'message': 'Successfully deleted.',
+      '_id': req.params.messageId
+    })
+  })
+  .catch((err) => {
+    throw err.message
+  })
+  // TODO: Return a JSON object indicating that the Message has been deleted
 })
 
 module.exports = router
--- src/test/message.js
+++ src/test/message.js
@@ -25,40 +25,127 @@ after((done) => {
   done()
 })
 
+const SAMPLE_OBJECT_ID_1 = "aaaaaaaaaaaa"
+const SAMPLE_OBJECT_ID_2 = "012345678910"
+const SAMPLE_OBJECT_ID_3 = "bbbbbbbbbbbb"
 
 describe('Message API endpoints', () => {
-    beforeEach((done) => {
-        // TODO: add any beforeEach code here
-        done()
+  beforeEach((done) => {
+    // TODO: add any beforeEach code here
+    const sampleUser = new User({
+      username: 'myuser',
+      password: 'mypassword',
+      _id: SAMPLE_OBJECT_ID_1
     })
 
-    afterEach((done) => {
-        // TODO: add any afterEach code here
-        done()
+    const sampleMessage = new Message({
+      title: 'mytitle',
+      body: 'mybody',
+      author: SAMPLE_OBJECT_ID_1,
+      _id: SAMPLE_OBJECT_ID_2
     })
 
-    it('should load all messages', (done) => {
-        // TODO: Complete this
-        done()
+    Promise.all([sampleUser.save(), sampleMessage.save()])
+    .then(() => {
+      done()
     })
+  })
 
-    it('should get one specific message', (done) => {
-        // TODO: Complete this
-        done()
-    })
+  afterEach((done) => {
+    // TODO: add any afterEach code here
+    deletion1 = User.deleteMany({ username: ['myuser'] })
+    deletion2 = Message.deleteMany({_id: SAMPLE_OBJECT_ID_2})
 
-    it('should post a new message', (done) => {
-        // TODO: Complete this
-        done()
+    Promise.all([deletion1, deletion2])
+    .then(() => {
+      done()
     })
+  })
 
-    it('should update a message', (done) => {
-        // TODO: Complete this
-        done()
+  it('should load all messages', (done) => {
+    // TODO: Complete this
+    chai.request(app)
+    .get('/messages')
+    .end((err, res) => {
+      if (err) { done(err) }
+      expect(res).to.have.status(200)
+      expect(res.body.messages).to.be.an("array")
+      done()
     })
+  })
 
-    it('should delete a message', (done) => {
-        // TODO: Complete this
-        done()
+  it('should get one specific message', (done) => {
+    // TODO: Complete this
+    chai.request(app)
+    .get(`/message/${SAMPLE_OBJECT_ID_2}`)
+    .end((err, res) => {
+      if (err) { done(err) }
+      expect(res).to.have.status(200)
+      expect(res.body).to.be.an('object')
+      expect(res.body.title).to.equal('mytitle')
+      expect(res.body.body).to.equal('mybody')
+      expect(res.body.author).to.equal(ObjectID(SAMPLE_USER_ID_2).toString())
+
+      done()
     })
+  })
+
+  it('should post a new message', (done) => {
+    // TODO: Complete this
+    chai.request(app)
+    .post('/messages')
+    .send({title: 'message',
+           body: 'body',
+           author: SAMPLE_OBJECT_ID_1,
+           _id: SAMPLE_OBJECT_ID_3})
+    .end((err, res) => {
+      if (err) { done(err) }
+      expect(res.body.message).to.be.an('object')
+      expect(res.body.message).to.have.property('title', 'message')
+      expect(res.body.message).to.have.property('body', 'body')
+
+      // check that message is actually inserted into database
+      Message.findOne({title: 'message'}).then(message => {
+        expect(message).to.be.an('object')
+        done()
+      })
+    })
+  })
+
+  it('should update a message', (done) => {
+    chai.request(app)
+    .put(`/messages/${SAMPLE_OBJECT_ID_2}`)
+    .send({title: 'anothermessage',
+           body: 'anotherbody'})
+    .end((err, res) => {
+      if (err) { done(err) }
+      expect(res.body.message).to.be.an('object')
+      expect(res.body.message).to.have.property('title', 'anothermessage')
+      expect(res.body.message).to.have.property('body', 'anotherbody')
+
+      // check that message is actually inserted into database
+      Message.findOne({title: 'anothermessage'}).then(message => {
+        expect(message).to.be.an('object')
+        expect(message).to.have.property('body', 'anotherbody')
+        done()
+      })
+    })
+  })
+
+  it('should delete a message', (done) => {
+    // TODO: Complete this
+    chai.request(app)
+    .delete(`/messages/${SAMPLE_OBJECT_ID_2}`)
+    .end((err, res) => {
+      if (err) { done(err) }
+      expect(res.body.message).to.equal('Successfully deleted.')
+      expect(res.body._id).to.equal(SAMPLE_OBJECT_ID_2)
+
+      // check that message is actually deleted from database
+      Message.findOne({title: 'mytitle'}).then(message => {
+        expect(message).to.equal(null)
+        done()
+      })
+    })
+  })
 })
